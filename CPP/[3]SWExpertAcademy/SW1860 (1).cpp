7e27d007875c8a6bd1014f7bc15dd6a8c445f367
minkyungcho <xiuperjjo@naver.com>
2019-10-24 19:36:24 +0900

    SW1860 수정

---
+++ CPP/[3]SWExpertAcademy/SW1860 (1).cpp
@@ -0,0 +1,80 @@
+#include <cstdio> 
+#include <cstring>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int T;
+int cnt;
+int N, M, K;
+int people[101]; // n초에 도착하는 사람을 people[n]에 저장
+int fish[101]; // n초마다 만든 붕어빵을 fish[n]에 저장
+
+void init();
+bool solve();
+void print();
+
+void init(){
+	scanf("%d", &T);
+    while(T--){
+        cnt++;
+    	//string ans = "";
+        scanf("%d %d %d", &N, &M, &K);
+        
+		queue<int> q;
+        // memset(people,0,101);
+        for(int i=0; i<N; i++){
+            int time;
+        	// scanf("%d", &people[i]);
+			scanf("%d", time);
+			q.push(time);
+        }
+		sort(q.begin(), q.end());
+        // sort(people+1, people+N);
+        //sort(people.begin(), people.end());
+        //print();
+        
+        
+        if(solve()){
+			printf("#%d Possible\n", cnt);        
+        }else {
+        	printf("#%d Impossible\n", cnt);      
+        }
+    }
+}
+
+bool solve(){
+     int fishnum = 0;
+    for(int i=1; i<=N; i++){
+    	//if(people[i] != 0){
+       // }
+        int t = people[i]; // 사람이 도착하는 시간(초)
+       
+        for(int j=1; j<=t; j++){
+        	fishnum += fish[j];
+        }
+        fishnum--;
+        if(fishnum<0){
+        	return false;
+        }
+    }
+	
+    return true;
+}
+void print(){
+	printf("%d\n", T);
+    printf("%d %d %d\n", N,M,K);
+    for(int i=1; i<=N; i++){
+        printf("%d ", people[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    init();
+  //  print();
+    
+    
+    return 0;
+}
